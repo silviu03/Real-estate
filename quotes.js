d332fe2df3fe82f939c7bc7dd1ef5903ef462efc
silviu03 <83119817+silviu03@users.noreply.github.com>
2021-09-05 19:48:12 +0300

    update

---
+++ quotes.js
@@ -0,0 +1,25 @@
+let quoteList = [
+     {
+        id: 1,
+        paragraph: "The road to success is always under construction."
+    },
+
+     {
+        id: 2,
+        paragraph: "It is not the beauty of a building you should look at; its the construction of the foundation that will stand the test of time."
+    },
+
+     {
+        id: 3, 
+        paragraph: "The purpose of art is not the release of a momentary ejection of adrenaline but is, rather, the gradual, lifelong construction of a state of wonder and serenity."
+    },
+     {
+        id: 4,
+        paragraph: "The whole difference between construction and creation is exactly this: that a thing constructed can only be loved after it is constructed; but a thing created is loved before it exists." 
+    },
+
+     {
+        id: 5,
+        paragraph: "If a building looks better under construction than it does when finished, then it's a failure."
+    }
+]
--- realestate.js
+++ realestate.js
@@ -1,91 +1,33 @@
-$(function() {
-    console.log('ready');
-
-    const plumbingService = $('.plumbing-service');
-    const electricityService = $('.electricity-service');
-    const carpentryService = $('.carpentry-service');
-    const decorationService = $('.decoration-service');
-    const refurbishmentsService = $('.refurbishments-service');
-    const extensionsService = $('.extensions-service');
-
-    const plumbing = $('.plumbing');
-    const electricity = $('.electricity');
-    const carpentry = $('.carpentry');
-    const decoration = $('.decoration');
-    const refurbishments = $('.refurbishments');
-    const extensions = $('.extensions');
+$(function () {
 
     const descriptionBox = $('.description-box');
+    let item = $('.item');
+    let quotes = $('.quotes');
+    let learnMore = $('.learn-more');
+
+    item.click(function (ev) {
+        let serviceId = $(this).data('service');
+        let selectedDescription = descriptionBox[serviceId - 1];
+        for (let i = 0; i < descriptionBox.length; i++) {
+            $(descriptionBox[i]).addClass('hidden');
+        }
+        $(selectedDescription).removeClass('hidden');
+    })
     
-    plumbingService.click(function() {
-        for(let i = 0; i < descriptionBox.length; i++) {
-            descriptionBox[i].classList.add('hidden')
-        }
-        if(plumbing.hasClass('hidden')) {
-            plumbing.removeClass('hidden')
-        } else {
-            return;
-        }
-
-    })
-    electricityService.click(function() {
-        for(let i = 0; i < descriptionBox.length; i++) {
-            descriptionBox[i].classList.add('hidden')
-        }
-        if(electricity.hasClass('hidden')) {
-            electricity.removeClass('hidden')
-        } else {
-            return;
-        }
-
-    })
-    carpentryService.click(function() {
-        for(let i = 0; i < descriptionBox.length; i++) {
-            descriptionBox[i].classList.add('hidden')
-        }
-        if(carpentry.hasClass('hidden')) {
-            carpentry.removeClass('hidden')
-        } else {
-            return;
-        }
-
-    })
-    decorationService.click(function() {
-        for(let i = 0; i < descriptionBox.length; i++) {
-            descriptionBox[i].classList.add('hidden')
-        }
-        if(decoration.hasClass('hidden')) {
-            decoration.removeClass('hidden')
-        } else {
-            return;
-        }
-
-    })
-    refurbishmentsService.click(function() {
-        for(let i = 0; i < descriptionBox.length; i++) {
-            descriptionBox[i].classList.add('hidden')
-        }
-        if(refurbishments.hasClass('hidden')) {
-            refurbishments.removeClass('hidden')
-        } else {
-            return;
-        }
-
-    })
-    extensionsService.click(function() {
-        for(let i = 0; i < descriptionBox.length; i++) {
-            descriptionBox[i].classList.add('hidden')
-        }
-        if(extensions.hasClass('hidden')) {
-            extensions.removeClass('hidden')
-        } else {
-            return;
-        }
-
+    getPropertyHTML = function(textMessage) {
+        return `<p> ${textMessage} </p>`
+    }
+    quoteListCategory = function(quoteList, randomNumberGnerator) {
+        let getRandomQuote = quoteList[randomNumberGnerator].paragraph;
+        let getRandomQuoteHTML = getPropertyHTML(getRandomQuote);
+        $('.quotes').html(getRandomQuoteHTML);
+    }
+    learnMore.click(function (event) {  
+        quoteListCategory(quoteList, randomNumberGnerator());
+        console.log(quoteList)
     })
 
-
-
-
-
+    randomNumberGnerator = function (){
+        return (Math.floor(Math.random()*5));
+     }
 })
